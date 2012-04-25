9a42fdc986766a2c08b754add41dd391aad38e7d
David Fekke <david@Davids-MacBook-Pro.local>
2012-04-24 22:09:15 -0400

    Initial Commit

---
+++ JaxArcSigHome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JaxArcSigHome
+//
+//  Created by David Fekke on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JaxArcSigHome/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  JaxArcSigHome
+//
+//  Created by David Fekke on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
